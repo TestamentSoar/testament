fe98d928ac087c8dc1a22e73b364592bd18454c9
Takuya <takuya.miraidenshi@gmail.com>
2019-11-09 19:16:57 +0900

    feat: add form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Main from './components/Main';
 import Cover from './components/Cover';
 import Yuigonshya from './components/Yuigonshya';
+import Sozokunin from './components/Sozokunin';
+import Zaisan from './components/Zaisan';
 import Explain from './components/WalkThrough/Explain';
 import Rules from './components/WalkThrough/Rules';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
@@ -16,6 +18,8 @@ function App() {
         <Route path="/rules" component={Rules}></Route>
         <Route path="/main" component={Main}></Route>
         <Route path="/yuigonshya" component={Yuigonshya}></Route>
+        <Route path="/sozokunin" component={Sozokunin}></Route>
+        <Route path="/zaisan" component={Zaisan}></Route>
       </Router>
     </div>
   );
