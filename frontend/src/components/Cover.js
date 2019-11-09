f66ab41b34ce0ee85d376a6d4363be08814e3c0d
mtdune <mtdune@s500.xrea.com>
2019-11-09 21:52:22 +0900

    Added Typography for cover.js

--- frontend/src/components/Cover.js
+++ frontend/src/components/Cover.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 
 const Cover = () => {
   return (
     <div>
-      <h1>ゆい言葉</h1>
-      <h2>遺言書作成アプリ</h2>
-      <h4>山岡法律事務所監修</h4>
-      <Link to="explain">進む</Link>
+      <br />
+      <br />
+      <br />
+      <Typography align='center' variant='h6' component='h2'>yuikotoba</Typography>
+      <Typography align='center' variant='h2' component='h1'>結言葉</Typography>
+      <br />
+      <br />
+      <Typography align='center' variant='h4' component='h2'>無料遺言書作成アプリ</Typography>
+      <Typography align='center' variant='h6' component='h2'>last will and testament app</Typography>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Typography align='center'>チームテスタメント法律事務所監修</Typography>
+      <Typography align='center'><Link to="explain">進む</Link></Typography>
     </div>
   );
 };
