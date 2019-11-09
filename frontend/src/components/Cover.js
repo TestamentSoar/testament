eafdd280dc71de2f13ab2e4cd22e4d43bcbf3028
mtdune <mtdune@s500.xrea.com>
2019-11-09 22:25:26 +0900

    Fixed some margins.

--- frontend/src/components/Cover.js
+++ frontend/src/components/Cover.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const theme = {
+  spacing: 8,
+}
 
 const Cover = () => {
   return (
     <div>
-      <br />
-      <br />
-      <br />
+      <Box m={10} />
       <Typography align='center' variant='h6' component='h2'>yuikotoba</Typography>
       <Typography align='center' variant='h2' component='h1'>結言葉</Typography>
-      <br />
-      <br />
+      <Box m={3} />
       <Typography align='center' variant='h4' component='h2'>無料遺言書作成アプリ</Typography>
       <Typography align='center' variant='h6' component='h2'>last will and testament app</Typography>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <Box m={40} />
       <Typography align='center'>チームテスタメント法律事務所監修</Typography>
       <Typography align='center'><Link to="explain">進む</Link></Typography>
     </div>
