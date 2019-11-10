3d972f34c99252338194dfcfb3405e9438f6400b
mtdune <mtdune@s500.xrea.com>
2019-11-10 14:16:39 +0900

    Added styling to /sozokunin.

--- frontend/src/components/Sozokunin/index.js
+++ frontend/src/components/Sozokunin/index.js
@@ -5,6 +5,16 @@ import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import {Link} from 'react-router-dom';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 class Sozokunin extends Component {
   constructor() {
@@ -25,25 +35,70 @@ class Sozokunin extends Component {
     return (
       <div>
         <FormControl>
-          <label>名前</label>
-          <Input name="name" value={this.state.inputs.name} />
-          <label>住所</label>
-          <Input name="alias" value={this.state.inputs.founded_date} />
-          <label>本籍</label>
-          <Input name="hobby" value={this.state.inputs['hobby']} />
-          <label>生年月日</label>
-          <Input name="point" value={this.state.inputs.point} />
-          <label>仕事</label>
-          <Input name="introduction" value={this.state.inputs.introduction} />
-          <label>関係</label>
-          <Input name="introduction" value={this.state.inputs.introduction} />
-          <label>価値予想</label>
-          <Input name="introduction" value={this.state.inputs.introduction} />
-          <Button variant="raised" onClick={this.handleSubmit}>
-            Submit
-          </Button>
+          <Grid container spacing={1} justify="center" alignItems="center">
+            <Box m={2} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <Link to="main">戻る</Link>
+              </Typography>
+            </Grid>
+            <Grid item xs={11}>
+              <Typography align='center' variant='h4' component='h1'>
+                相続人の登録
+              </Typography>
+            </Grid>
+            <Box m={1} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <label>名前</label>
+                <Input name="name" value={this.state.inputs.name} />
+              </Typography>
+            </Grid>
+            <Box m={2} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <label>住所</label>
+                <Input name="alias" value={this.state.inputs.founded_date} />
+              </Typography>
+            </Grid>
+            <Box m={2} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <label>本籍</label>
+                <Input name="hobby" value={this.state.inputs['hobby']} />
+              </Typography>
+            </Grid>
+            <Box m={2} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <label>生年月日</label>
+                <Input name="point" value={this.state.inputs.point} />
+              </Typography>
+            </Grid>
+            <Box m={2} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <label>職業</label>
+                <Input name="introduction" value={this.state.inputs.introduction} />
+              </Typography>
+            </Grid>
+            <Box m={2} />
+            <Grid item xs={11}>
+              <Typography align='left' variant='h6' component='h2'>
+                <label>続柄</label>
+                <Input name="introduction" value={this.state.inputs.introduction} />
+              </Typography>
+            </Grid>
+            <Box m={2} />
+            <Grid item xs={11}>
+             <Typography align='center' variant='h1' component='h1'>
+                <Button variant="text" onClick={this.handleSubmit}>
+                  登録
+                </Button>
+              </Typography>
+            </Grid>
+          </Grid>
         </FormControl>
-        <Link to="main">戻る</Link>
       </div>
     );
   }
